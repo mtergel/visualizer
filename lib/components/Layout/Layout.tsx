9bc649707e7392914e348c4ef3335068ed5e2b8d
Tergel Munkhdelger <tergelm@gmail.com>
2022-02-07 13:40:24 +0800

    icon button

--- lib/components/Layout/Layout.tsx
+++ lib/components/Layout/Layout.tsx
@@ -1,4 +1,7 @@
 import { HiOutlineTranslate as MainIcon } from "@react-icons/all-files/hi/HiOutlineTranslate";
+import { HiMenu } from "@react-icons/all-files/hi/HiMenu";
+import IconButton from "components/IconButton/IconButton";
+
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
@@ -13,7 +16,13 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 const Header: React.FC<{}> = () => {
   return (
     <header className="flex items-center h-16 border-b">
-      <div className="w-[72px] flex-shrink-0 h-full text-center lg:w-[60px]"></div>
+      <div className="w-[72px] flex items-center justify-center flex-shrink-0 h-full text-center lg:w-[60px]">
+        <IconButton
+          aria-label="toggle sidemenu"
+          icon={<HiMenu />}
+          variant="ghost"
+        />
+      </div>
       <h1 className="flex flex-row items-center gap-2">
         <span>
           <MainIcon className="h-6 w-6 text-indigo-300" />

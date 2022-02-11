00a42ff8ce9c94e5862d53de473a5a16b5b79837
Tergel Munkhdelger <tergelm@gmail.com>
2022-02-11 17:44:24 +0800

    sub header layout

--- pages/sorting.tsx
+++ pages/sorting.tsx
@@ -1,8 +1,15 @@
 import Button from "components/Button/Button";
+import IconButton from "components/IconButton/IconButton";
 import Slider from "components/Slider/Slider";
-import { useCallback, useEffect, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { useResizeDetector } from "react-resize-detector";
 import { generateRandomArray } from "utils/arrayUtils";
+import { HiCog } from "@react-icons/all-files/hi/HiCog";
+import Dialog, {
+  DialogClose,
+  DialogDescription,
+  DialogTitle,
+} from "components/Dialog/Dialog";
 
 interface SortingProps {}
 
@@ -25,6 +32,7 @@ const Sorting: React.FC<SortingProps> = () => {
     { id: string; value: number }[] | null
   >(null);
   const [arrayLen, setArrayLen] = useState(ARRAY_LEN);
+  const [sortingAlgo, setSortingAlgo] = useState<AlgoKey>(AlgoKey.BUBBLE);
   const { width, height, ref } = useResizeDetector();
 
   useEffect(() => {
@@ -35,16 +43,21 @@ const Sorting: React.FC<SortingProps> = () => {
     setRandomArray(generateRandomArray(arrayLen, ARRAY_LOWER, ARRAY_UPPER));
   }, [arrayLen]);
 
+  const handleAlgoChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortingAlgo(e.currentTarget.value as AlgoKey);
+  };
+
   return (
     <div className="flex flex-col h-full">
-      <div className="h-16 bg-skin-secondary border-b">
+      <div className="h-[4.5rem] bg-skin-secondary border-b">
         <div className="max-w-screen-2xl mx-auto flex items-center h-full px-4 md:px-[60px]">
-          <div className="flex items-center flex-grow gap-3">
-            <Button variant="outline" onClick={handleGenerateNew}>
-              Generate
-            </Button>
-            <div className="max-w-[144px] w-full flex-shrink-0">
+          <div className="items-center flex-grow gap-3 hidden sm:flex">
+            <div className="max-w-[144px] w-full flex-shrink-0 flex flex-col">
+              <label htmlFor="arrayLength" className="text-xs text-skin-muted">
+                Size
+              </label>
               <Slider
+                id="arrayLength"
                 min={10}
                 max={ARRAY_MAX}
                 step={10}
@@ -52,14 +65,91 @@ const Sorting: React.FC<SortingProps> = () => {
                   setArrayLen(val[0]);
                   handleGenerateNew();
                 }}
-                name="array_length"
+                name="Array length"
                 value={[arrayLen]}
               />
             </div>
+            <label htmlFor=""></label>
+            <select
+              aria-label="select sorting algorithm"
+              value={sortingAlgo}
+              onChange={handleAlgoChange}
+              className="h-10 bg-transparent border border-skin-base rounded-lg font-semibold focus:ring focus:ring-gray-300"
+            >
+              {Object.entries(AlgoKey).map((pair) => (
+                <option key={pair[0]} value={pair[1]} className="font-normal">
+                  {pair[1]}
+                </option>
+              ))}
+            </select>
           </div>
 
-          <div className="flex-shrink-0 flex items-center justify-end">
-            <Button color="primary">Sort</Button>
+          <div className="flex-shrink-0 flex items-center justify-end gap-2">
+            <Dialog
+              contentClassname="flex flex-col"
+              content={
+                <div className="h-full flex flex-col">
+                  <div className="pt-5 px-6 pb-4 flex-grow">
+                    <DialogTitle className="dialog-title">
+                      Sort options
+                    </DialogTitle>
+                    <DialogDescription className="dialog-description">
+                      You can tweak the settings of the visualization here.
+                    </DialogDescription>
+                    <div className="flex flex-col gap-6 my-3">
+                      <div className="w-full flex-shrink-0 flex flex-col">
+                        <label className="text-sm text-skin-muted">Size</label>
+                        <Slider
+                          min={10}
+                          max={ARRAY_MAX}
+                          step={10}
+                          onValueChange={(val) => {
+                            setArrayLen(val[0]);
+                            handleGenerateNew();
+                          }}
+                          name="Array length"
+                          value={[arrayLen]}
+                        />
+                      </div>
+                      <select
+                        aria-label="select sorting algorithm"
+                        value={sortingAlgo}
+                        onChange={handleAlgoChange}
+                        className="bg-transparent border border-skin-base rounded-lg font-semibold focus:ring focus:ring-gray-300"
+                      >
+                        {Object.entries(AlgoKey).map((pair) => (
+                          <option
+                            key={pair[0]}
+                            value={pair[1]}
+                            className="font-normal"
+                          >
+                            {pair[1]}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+                  <div className="bg-skin-offset px-6 pb-4 w-full">
+                    <DialogClose asChild>
+                      <Button isFullWidth color="primary" className="px-8">
+                        Sort
+                      </Button>
+                    </DialogClose>
+                  </div>
+                </div>
+              }
+            >
+              <IconButton
+                color="primary"
+                aria-label="settings"
+                className="inline-flex sm:hidden"
+                icon={<HiCog />}
+              />
+            </Dialog>
+
+            <Button color="primary" className="px-8">
+              Sort
+            </Button>
           </div>
         </div>
       </div>

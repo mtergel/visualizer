442c6874467614dccef269a54b2cb4843cb62d44
Tergel Munkhdelger <tergelm@gmail.com>
2022-02-18 10:39:05 +0800

    cell dragging, little laggy on fade

--- pages/pathfinding.tsx
+++ pages/pathfinding.tsx
@@ -1,9 +1,12 @@
 import { AiFillCar } from "@react-icons/all-files/ai/AiFillCar";
 import { FaFlagCheckered } from "@react-icons/all-files/fa/FaFlagCheckered";
 import clsx from "clsx";
+import IconButton from "components/IconButton/IconButton";
 import memoize from "memoize-one";
-import { memo, useState } from "react";
+import React, { memo, useState } from "react";
 import { areEqual, FixedSizeGrid as Grid } from "react-window";
+import { RiPencilRuler2Line } from "@react-icons/all-files/ri/RiPencilRuler2Line";
+import * as Toolbar from "@radix-ui/react-toolbar";
 
 interface PathFindingProps {}
 
@@ -16,6 +19,15 @@ const enum NodeType {
   "End",
   "Wall",
 }
+type DragNode = {
+  type: NodeType;
+  row: number;
+  col: number;
+  initRow: number;
+  initCol: number;
+};
+
+const DraggableNodes = [NodeType.Start, NodeType.End];
 
 let initialGrid = Array.from({ length: ROWS }, () =>
   Array.from({ length: COLS }, () => NodeType.Normal)
@@ -24,34 +36,49 @@ let initialGrid = Array.from({ length: ROWS }, () =>
 initialGrid[4][4] = NodeType.Start;
 initialGrid[12][36] = NodeType.End;
 
-const wallColor = "bg-gray-600 dark:bg-gray-400";
+const wallColor = "bg-gray-600 dark:bg-gray-500";
 
-const createItemData = memoize((grid, isMouseDown, handleSetGrid) => ({
-  grid,
-  isMouseDown,
-  handleSetGrid,
-}));
+const createItemData = memoize(
+  (grid, isMouseDown, dragNode, handleSetGrid, handleSetDragNode) => ({
+    grid,
+    isMouseDown,
+    dragNode,
+    handleSetGrid,
+    handleSetDragNode,
+  })
+);
 
 const PathFinding: React.FC<PathFindingProps> = () => {
   const [grid, setGrid] = useState(initialGrid);
   const [isMouseDown, setIsMouseDown] = useState(false);
+  const [dragNode, setDragNode] = useState<DragNode | null>(null);
 
   const handleSetGrid = (grid: NodeType[][]) => {
     setGrid(grid);
   };
+  const handleSetDragNode = (node: DragNode | null) => {
+    setDragNode(node);
+  };
 
-  const itemData = createItemData(grid, isMouseDown, handleSetGrid);
+  const itemData = createItemData(
+    grid,
+    isMouseDown,
+    dragNode,
+    handleSetGrid,
+    handleSetDragNode
+  );
 
   return (
     <div className="flex flex-col h-full">
       <div className="h-[4.5rem] bg-skin-secondary border-b">
-        <div className="max-w-screen-2xl mx-auto flex items-center h-full px-4 md:px-[60px]">
-          SUB HEADER
-        </div>
+        <div className="max-w-screen-2xl mx-auto flex items-center h-full px-4 md:px-[60px]"></div>
       </div>
       <div className="flex-grow">
         <div
-          className="h-full w-full flex items-center justify-center"
+          className={clsx(
+            "h-full w-full flex items-center justify-center select-none",
+            dragNode && "cursor-grabbing"
+          )}
           onMouseDown={() => setIsMouseDown(true)}
           onMouseUp={() => setIsMouseDown(false)}
         >
@@ -76,44 +103,110 @@ const Cell = memo((props: any) => {
   const {
     grid,
     isMouseDown,
+    dragNode,
     handleSetGrid,
+    handleSetDragNode,
   }: {
     grid: NodeType[][];
     isMouseDown: boolean;
+    dragNode: DragNode | null;
     handleSetGrid: (grid: NodeType[][]) => void;
+    handleSetDragNode: (node: DragNode | null) => void;
   } = props.data;
   const { columnIndex, rowIndex }: { columnIndex: number; rowIndex: number } =
     props;
   const cell = grid[rowIndex][columnIndex];
   const handleMouseOver = () => {
     if (isMouseDown) {
+      if (dragNode) {
+        handleSetDragNode({
+          ...dragNode,
+          col: columnIndex,
+          row: rowIndex,
+        });
+      } else if (cell === NodeType.Normal) {
+        let copy = [...grid];
+        copy[rowIndex][columnIndex] = NodeType.Wall;
+        handleSetGrid(copy);
+      }
+    }
+  };
+  const handleMouseDown = () => {
+    if (cell === NodeType.Normal) {
+      // set normal to wall
       let copy = [...grid];
       copy[rowIndex][columnIndex] = NodeType.Wall;
       handleSetGrid(copy);
+    } else if (DraggableNodes.includes(cell)) {
+      handleSetDragNode({
+        type: cell,
+        col: columnIndex,
+        row: rowIndex,
+        initRow: rowIndex,
+        initCol: columnIndex,
+      });
     }
   };
+
+  const handleMouseUp = () => {
+    if (dragNode && isMouseDown) {
+      if (DraggableNodes.includes(cell)) {
+        handleSetDragNode(null);
+        return;
+      }
+
+      let copy = [...grid];
+      copy[dragNode.initRow][dragNode.initCol] = NodeType.Normal;
+      copy[rowIndex][columnIndex] = dragNode.type;
+      handleSetGrid(copy);
+      handleSetDragNode(null);
+    }
+  };
+
+  const isDraggingNodeHere =
+    dragNode && rowIndex === dragNode.row && columnIndex === dragNode.col;
 
   return (
     <div
       style={props.style}
-      onMouseOver={cell === NodeType.Normal ? handleMouseOver : undefined}
+      onMouseDown={handleMouseDown}
+      onMouseOver={handleMouseOver}
+      onMouseUp={handleMouseUp}
       className={clsx(
-        "border-t border-l flex items-center justify-center",
+        "border-t border-l flex items-center justify-center transition-colors",
         props.rowIndex === ROWS - 1 && "border-b",
         props.columnIndex === COLS - 1 && "border-r",
-        cell === NodeType.Wall && wallColor
+        !isDraggingNodeHere && cell === NodeType.Wall && wallColor,
+        DraggableNodes.includes(cell) && "cursor-grab",
+        isDraggingNodeHere && "bg-gray-100/20 opacity-60"
       )}
     >
-      {cell === NodeType.Start && (
-        <AiFillCar className="h-7 w-7 dark:text-indigo-300" />
-      )}
-
-      {cell === NodeType.End && (
-        <FaFlagCheckered className="h-6 w-6 text-green-700 dark:text-green-300" />
+      {isDraggingNodeHere ? (
+        <RenderNodeType nodeType={dragNode.type} />
+      ) : (
+        <RenderNodeType nodeType={cell} />
       )}
     </div>
   );
 }, areEqual);
+
+const RenderNodeType: React.FC<{ nodeType: NodeType }> = ({ nodeType }) => {
+  switch (nodeType) {
+    case NodeType.Start: {
+      return <AiFillCar className="h-7 w-7 dark:text-indigo-300" />;
+    }
+
+    case NodeType.End: {
+      return (
+        <FaFlagCheckered className="h-6 w-6 text-green-700 dark:text-green-300" />
+      );
+    }
+
+    default: {
+      return null;
+    }
+  }
+};
 
 Cell.displayName = "GridCell";
 export default PathFinding;
